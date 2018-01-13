ee79ba97d485de6c7793bdc09908573ace451fb8
JBecker89 <jazmyn.becker@gmail.com>
2018-01-13 14:17:26 -0600

    fixing README.md description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject chatter "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "FIXME: HI you are doing great"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
@@ -9,4 +9,4 @@
   :ring {:handler chatter.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.0"]]}})
+                        [ring/ring-mock "0.3.0"]]}}
